56e2a6dae1c48d939f221176765e8cb2f2eb33e2
Dev50 <franciscotortillol@gmail.com>
2025-12-19 08:40:43 +0100

    git commit -m "ch01: Estructura inicial del proyecto y test de arranque"

---
+++ src/test/java/com/example/socialnetwork/ApplicationStartupIT.java
@@ -0,0 +1,8 @@
+package com.example.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationStartupIT {
+    @Test// Verify that the Spring context boots correctly
+    void contextLoads(){}
+}
