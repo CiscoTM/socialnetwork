1b71c2be0752f010c347e306efc15adf9f5ffd79
Dev50 <franciscotortillol@gmail.com>
2025-12-27 18:40:39 +0100

    ch13: Servicios de aplicación de comentarios, likes y follows con tests unitarios

---
+++ src/main/java/com/example/socialnetwork/application/interactions/comment/CreateCommentService.java
@@ -0,0 +1,21 @@
+package com.example.socialnetwork.application.interactions.comment;
+
+import com.example.socialnetwork.domain.interactions.comment.Comment;
+import com.example.socialnetwork.domain.interactions.comment.CommentContent;
+import com.example.socialnetwork.domain.interactions.comment.CommentId;
+import com.example.socialnetwork.domain.interactions.comment.ports.CommentRepository;
+import com.example.socialnetwork.domain.post.AuthorId;
+import com.example.socialnetwork.domain.post.PostId;
+
+public class CreateCommentService {
+    private final CommentRepository commentRepository;
+
+    public CreateCommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+    public Comment execute(AuthorId authorId, PostId postId, String content){
+        Comment comment = Comment.create(CommentId.generate(), authorId, postId, CommentContent.of(content));
+        commentRepository.save(comment);
+        return comment;
+    }
+}
